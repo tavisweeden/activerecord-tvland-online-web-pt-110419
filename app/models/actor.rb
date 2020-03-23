7c0ecedecd62ad21909283d177d36dda40a44759
github username <github email address>
2020-03-23 14:31:43 -0400

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+      self.first_name + " " + self.last_name
+        
+    end
+
+    def list_roles
+      show_name = self.shows.pluck(:name).join
+      char_name = self.characters.pluck(:name).join
+      char_name + ' - ' + show_name
+
+    end
   
 end
